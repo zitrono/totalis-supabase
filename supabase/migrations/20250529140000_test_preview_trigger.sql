580f856fea283e0d58424cd83096a9b836c3af9a
zitrono <zitrono@gmail.com>
2025-05-29 15:15:11 +0200

    test: Add migration to trigger preview branch

---
+++ supabase/migrations/20250529140000_test_preview_trigger.sql
@@ -0,0 +1,4 @@
+-- Test migration to trigger preview branch creation
+-- This migration adds a comment to verify preview branch functionality
+
+COMMENT ON TABLE profiles IS 'User profiles table - testing preview branch trigger';
