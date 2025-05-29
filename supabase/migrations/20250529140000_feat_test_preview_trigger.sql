2b81d77664c84f03e0dc677b9d07fa96ea4ff6f7
zitrono <zitrono@gmail.com>
2025-05-29 16:07:11 +0200

    cleanup: Remove test files

--- supabase/migrations/20250529140000_feat_test_preview_trigger.sql
+++
@@ -1,4 +0,0 @@
--- Test migration to trigger preview branch creation
--- This migration adds a comment to verify preview branch functionality
-
-COMMENT ON TABLE profiles IS 'User profiles table - testing preview branch trigger';
